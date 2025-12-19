a8d9942e0c5cc38e43f3ee549e389b6f89015c38
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2025-12-19 21:19:26 +0100

    Add several new macros

--- main.rkt
+++ main.rkt
@@ -1,8 +1,14 @@
 #lang racket
-(require (rename-in racket/base (+ base:+) (= base:=) (- base:-) (substring base:substring)))
 (require (rename-in racket/class (new base:new) (object% base:object%)))
 (require racket/match)
 (require racket/generator)
+(require (for-syntax racket/syntax))
+(require syntax/parse (for-syntax syntax/parse))
+(require
+    threading
+    data/collection)
+(require (rename-in (only-in racket/base + = - substring) 
+             (+ base:+) (= base:=) (- base:-) (substring base:substring)))
 
 (provide + - = 
   char->number 
@@ -12,7 +18,13 @@
   new
   in-string
   generator
-  for/match)
+  for/match
+  for/list/match
+  lambda/match
+  bytes->string/utf-16
+  yield
+  (all-from-out threading)
+  (all-from-out data/collection))
 
 (define-syntax-rule (substring . args)
   (with-handlers ([exn:fail:contract? (lambda (e) "")])
@@ -40,14 +52,6 @@
         [(andmap char? args) (apply char=? args)]
         [(andmap symbol? args) (apply symbol=? args)]
         [else (apply base:= args)]))
-
-(define-syntax-rule (ref container . args)
-  (let 
-    ([func (cond [(list? container) list-ref]
-                 [(hash? container) hash-ref]
-                 [(vector? container) vector-ref]
-                 [(string? container) string-ref])])
-    (func container . args)))
 
 (define-syntax-rule (with-input filename . rest)
     (with-input-from-file filename
@@ -83,19 +87,48 @@
   (in-list (string->list str)))
 
 (define-syntax (generator stx)
+    (define-syntax-class maybe-arity
+      (pattern (#:arity arity)))
     (syntax-parse stx
-        [(_ body ...)
-         #'(sequence->stream (in-generator body ...))]))
+        [(_ (~optional maybe-arity) body ...)
+         #'(sequence->stream (in-generator (~? maybe-arity) body ...))]))
+
+(begin-for-syntax
+  (define (generic-for/match stx for-func)
+    (define-splicing-syntax-class maybe-break
+      (pattern (~seq #:break break)
+               #:with res #'(#:break break))
+      (pattern (~seq #:final final)
+               #:with res #'(#:final final))
+      (pattern (~seq)
+               #:with res #'()))
+    (syntax-parse stx
+        [(_ ([pat seq-expr]) maybe-break:maybe-break body ...)
+         #:with x (generate-temporary)
+         #`(#,for-func ([x seq-expr])
+            (~@ . maybe-break.res)
+            (match x
+              [pat body ...]))])))
 
 (define-syntax (for/match stx)
-    (syntax-parse stx
-        [(_ ([pat seq-expr]) body ...)
-         #:with x (generate-temporary)
-         #'(for ([x seq-expr])
-              (match x
-               [pat body ...]))]))
+  (generic-for/match stx #'for))
 
+(define-syntax (for/list/match stx)
+  (generic-for/match stx #'for/list))
 
+(define-syntax (lambda/match stx)
+  (syntax-parse stx
+    [(_ pat body ...)
+     #:with x (generate-temporary)
+     #'(lambda (x)
+         (match x
+           [pat body ...]))]))
+
+(define (bytes->string/utf-16 bytes)
+    (define converter (bytes-open-converter "UTF-16le" "UTF-8"))
+    (let-values ([(bytes1 x y) (bytes-convert converter bytes)]) 
+         (bytes->string/utf-8 bytes1)))
+  
 (define mylist (list 1 2 3))
 (define mymap (make-hash '[("a" . 1) ("b" . 2) ("c" . 3)]))
 (define myvector #(1 2 3))
